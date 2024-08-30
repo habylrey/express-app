07c9a94ed654aa303271838c8663a8501f581295
habylrey <habylrey@gmail.com>
2024-08-30 10:05:35 +0300

    Repeated functions from user.service.js, order.service.js, group.service.js are moved to a separate service.js file

--- src/order/order.service.js
+++ src/order/order.service.js
@@ -1,54 +1,23 @@
-import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import * as service from '../services/service.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const ordersPath = path.join(__dirname, '..', 'data', 'orders.json');
 
-class OrderService {
-	async getAllOrders() {
-		const data = await fs.readFile(ordersPath, 'utf8');
-		return JSON.parse(data);
-	}
+const getAllOrders = () => service.getAll(ordersPath);
+const getOrderById = (id) => service.getById(ordersPath, id);
+const createOrder = (orderData) => service.create(ordersPath, orderData);
+const updateOrder = (id, orderData) =>
+	service.update(ordersPath, id, orderData);
+const deleteOrder = (id) => service.remove(ordersPath, id);
 
-	async getOrderById(id) {
-		const orders = await this.getAllOrders();
-		return orders.find((order) => order.id === parseInt(id));
-	}
-
-	async createOrder(orderData) {
-		const orders = await this.getAllOrders();
-		const newOrder = {
-			id: orders.length + 1,
-			...orderData,
-		};
-		orders.push(newOrder);
-		await fs.writeFile(ordersPath, JSON.stringify(orders, null, 2));
-		return newOrder;
-	}
-
-	async updateOrder(id, orderData) {
-		const orders = await this.getAllOrders();
-		const index = orders.findIndex((order) => order.id === parseInt(id));
-		if (index === -1) return null;
-
-		orders[index] = { ...orders[index], ...orderData };
-		await fs.writeFile(ordersPath, JSON.stringify(orders, null, 2));
-		return orders[index];
-	}
-
-	async deleteOrder(id) {
-		const orders = await this.getAllOrders();
-		const filteredOrders = orders.filter(
-			(order) => order.id !== parseInt(id)
-		);
-		if (filteredOrders.length === orders.length) return false;
-
-		await fs.writeFile(ordersPath, JSON.stringify(filteredOrders, null, 2));
-		return true;
-	}
-}
-
-export default new OrderService();
+export default {
+	getAllOrders,
+	getOrderById,
+	createOrder,
+	updateOrder,
+	deleteOrder,
+};
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,52 +1,22 @@
-import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import * as service from '../services/service.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
-class UserService {
-	async getAllUsers() {
-		const data = await fs.readFile(usersPath, 'utf8');
-		return JSON.parse(data);
-	}
+const getAllUsers = () => service.getAll(usersPath);
+const getUserById = (id) => service.getById(usersPath, id);
+const createUser = (userData) => service.create(usersPath, userData);
+const updateUser = (id, userData) => service.update(usersPath, id, userData);
+const deleteUser = (id) => service.remove(usersPath, id);
 
-	async getUserById(id) {
-		const users = await this.getAllUsers();
-		return users.find((user) => user.id === parseInt(id));
-	}
-
-	async createUser(userData) {
-		const users = await this.getAllUsers();
-		const newUser = {
-			id: users.length + 1,
-			...userData,
-		};
-		users.push(newUser);
-		await fs.writeFile(usersPath, JSON.stringify(users, null, 2));
-		return newUser;
-	}
-
-	async updateUser(id, userData) {
-		const users = await this.getAllUsers();
-		const index = users.findIndex((user) => user.id === parseInt(id));
-		if (index === -1) return null;
-
-		users[index] = { ...users[index], ...userData };
-		await fs.writeFile(usersPath, JSON.stringify(users, null, 2));
-		return users[index];
-	}
-
-	async deleteUser(id) {
-		const users = await this.getAllUsers();
-		const filteredUsers = users.filter((user) => user.id !== parseInt(id));
-		if (filteredUsers.length === users.length) return false;
-
-		await fs.writeFile(usersPath, JSON.stringify(filteredUsers, null, 2));
-		return true;
-	}
-}
-
-export default new UserService();
+export default {
+	getAllUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+};
