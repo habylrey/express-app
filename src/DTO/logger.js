ac9385a3a87b6f4e2939b6de4e9ba2192c245191
habylrey <habylrey@gmail.com>
2024-09-05 11:33:27 +0300

    logger middleware refactored

--- src/DTO/logger.js
+++ src/DTO/logger.js
@@ -6,10 +6,6 @@ class Logger {
 	static error(message) {
 		console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
 	}
-
-	static debug(message) {
-		console.debug(`[DEBUG] ${new Date().toISOString()} - ${message}`);
-	}
 }
 
 export default Logger;
